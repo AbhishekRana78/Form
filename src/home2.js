eccc4e250449cf4e6a85a532425957c4d8338c4a
AbhishekRana78 <149921242+AbhishekRana78@users.noreply.github.com>
2024-02-21 20:54:47 +0530

    Add files via upload

---
+++ src/home2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./home2.css";
+
+function Home2() {
+  return (
+    <div class="hello">
+      <h2>Your Details: </h2>
+      <p>Name: Abhishek Rana</p>
+      <p>Age: 22</p>
+      <p>Address:lpu,phagwara,punjab</p>
+      <p>Email: abhishekrana@gmail.com</p>
+    </div>
+  );
+}
+
+export default Home2;
